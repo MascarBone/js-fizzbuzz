eb7b0907c40b9c6756c0a48a08fa9badafd0e2b1
MascarBone <simone.bonelli@gmail.com>
2021-08-25 15:39:42 +0200

    Update FizzBuzz, HTML Bootstrap with columns

--- js/script.js
+++ js/script.js
@@ -17,17 +17,44 @@
 
 for (let i = 1; i <= 100; i++)
 {
+    /*
+    // Condizione per verificare di essere al primo valore della riga.
+    // Con il controllo i==1, il primo inserimento sarà sempre un offset-1
+    // Con il controllo (i-1)%10==0 verifico che ci troviamo ad un multiplo di 10,
+    // inserendo un altro offset precisamente dopo la decima colonna
+
+    con gli offset c'è qualcosa di non chiaro sul fuzionamento con i margini, in caso utilizzare justify-content-center sulla row
+
+    if ( i==1 || (i-1)%10==0 )
+    {
+        document.getElementById('numeri').innerHTML += '<span class="offset-1"></span>';
+    }
+    */
+    
+    // Condizione per controllare di essere all'inizio del condice HTML per inserire il primo spazio vuoto
+    if ( i==1)
+    {
+        document.getElementById('numeri').innerHTML += '<span class="col-1"></span>';
+    }
+    // Condizione che ogni 10 valori inseriti, crea due spazi vuoti per allinearli con il resto della tabella
+    if ( i > 1 && (i-1)%10==0 )
+    {
+        document.getElementById('numeri').innerHTML += '<span class="col-1"></span>';
+        document.getElementById('numeri').innerHTML += '<span class="col-1"></span>';
+    }
+
+    // Serie di Condizioni per verificare che i numeri siano multipli di 3 e 5, o nel caso di uno o l'altro
     if ( (i%3==0) && (i%5==0) )
     {
-        document.getElementById('numeri').innerHTML += '<span class="me-2">FizzBuzz</span>';
+        document.getElementById('numeri').innerHTML += '<span class="col-1">FizzBuzz</span>';
     }else if (i%3==0)
     {
-        document.getElementById('numeri').innerHTML += '<span class="me-2">Fizz</span>';
+        document.getElementById('numeri').innerHTML += '<span class="col-1">Fizz</span>';
     }else if (i%5==0)
     {
-        document.getElementById('numeri').innerHTML += '<span class="me-2">Buzz</span>';
+        document.getElementById('numeri').innerHTML += '<span class="col-1">Buzz</span>';
     }else
     {
-        document.getElementById('numeri').innerHTML += '<span class="me-2">' + i + '</span>';
+        document.getElementById('numeri').innerHTML += '<span class="col-1">' + i + '</span>';
     }
 }
